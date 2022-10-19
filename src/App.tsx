e7bffde52aeadfda4af5ed7b36b3259fbbd3f0e6
Dong-Yeop <ehdduq53@gmail.com>
2022-10-19 17:39:53 +0900

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import Circle from './Circle';
+import React from 'react';
+import styled from 'styled-components';
+import FormEvent from './FormEvent';
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" borderColor="tomato" />
-      <Circle bgColor="tomato" />
+      <FormEvent />
+      <Container>
+        <H1>protected</H1>
+      </Container>
     </div>
   );
 }
