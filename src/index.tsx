e71f87177186944ed0d7501e745b4c6a23553f7d
Dong-Yeop <ehdduq53@gmail.com>
2022-10-27 19:39:26 +0900

    set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { lightTheme, darkTheme } from './theme';
+import { darkTheme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
 );
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,11 +1,10 @@
-// import original module declarations
 import 'styled-components';
 
-// and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
     textColor: string;
     bgColor: string;
-    btnColor: string;
+    accentColor: string;
+    cardBgColor: string;
   }
 }
